d70fa8d0ef400ef9ac7b2e8f52e7263d6a41b952
Joshua Franco <francoswim@yahoo.com>
2025-09-06 17:43:46 -0400

    updating mobile

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,17 @@
-const menuBtn = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('nav.nav-links');
+const dropdownToggles = document.querySelectorAll('.dropdown-toggle');
 
-// Mobile menu open/close
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-  navLinks.classList.toggle("open");
+// Hamburger open/close
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('open');
+    navLinks.classList.toggle('open');
 });
 
-// Mobile dropdown toggle
-document.querySelectorAll(".dropdown-toggle").forEach(toggle => {
-  toggle.addEventListener("click", (e) => {
-    e.preventDefault(); // prevent default button behavior
-    const dropdown = toggle.parentElement;
-    dropdown.classList.toggle("active");
-  });
+// Mobile dropdown open/close
+dropdownToggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        const dropdown = toggle.parentElement;
+        dropdown.classList.toggle('active');
+    });
 });
